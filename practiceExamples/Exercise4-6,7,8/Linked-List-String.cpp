2c5637e653b524e0f30809dedd089860db5ec667
ajl4000 <alexlennartsson@gmail.com>
2023-06-15 22:48:18 +0100

    finished linked list string implementation

--- practiceExamples/Exercise4-6,7,8/Linked-List-String.cpp
+++ practiceExamples/Exercise4-6,7,8/Linked-List-String.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+struct stringNode* removeChars(struct stringNode* s, int pos, int length);
 void concatenate(struct stringNode* s1, struct stringNode* s2);
 void append(struct stringNode* s, char c);
 char characterAt(struct stringNode* s, int index);
@@ -16,21 +17,66 @@ struct stringNode{
 int main(void) {
     char s1[] = "Hello"; 
     struct stringNode* head = createListFromString(s1, 5);
+    char s2[] = "World!"; 
+    struct stringNode* world = createListFromString(s2, 6);
     printString(head);
     cout << '\n';
     cout << characterAt(head, 4) << '\n';
     append(head, '!');
     printString(head);
+    cout << '\n';
+    concatenate(head, world);
+    printString(head);
+    cout << '\n';
+    head = removeChars(head, 1, 6);
+    printString(head);
+    cout << '\n';
+
 
     return 1;
 }
 
-void concatenate(struct stringNode* s1, struct stringNode* s2) {
-    struct stringNode* curr1 = s1;
-    while(curr1->next != NULL) {
-        curr1 = curr1->next;
+struct stringNode* removeChars(struct stringNode* s, int pos, int length) {
+    struct stringNode* nodePtr = s;
+    struct stringNode* trailing = NULL;
+    int i = 1;
+    while(nodePtr != NULL && i != pos) {
+        trailing = nodePtr;
+        nodePtr = nodePtr->next;
+        i++;
     }
+    int j = 1;
+    while(j <= length && nodePtr != NULL) {
+        struct stringNode* temp = nodePtr;
+        if(i == 1) {
+            nodePtr = nodePtr->next;
+            s = nodePtr;
+            delete temp;
+        }
+        else {
+            trailing->next = nodePtr->next;
+            nodePtr = nodePtr->next;
+            delete temp;
+        }
+        j++;
+    }
+    return s;
+}
 
+void concatenate(struct stringNode* s1, struct stringNode* s2) {
+    struct stringNode* nodePtr = s1;
+    while(nodePtr->next != NULL) {
+        nodePtr = nodePtr->next;
+    }
+    struct stringNode* copyPtr = s2;
+    while(copyPtr != NULL) {
+        struct stringNode* newNode = new stringNode;
+        newNode->letter = copyPtr->letter;
+        nodePtr->next = newNode;
+        nodePtr = nodePtr->next;
+        copyPtr = copyPtr->next;
+    }
+    nodePtr->next = NULL;
 }
 
 void append(struct stringNode* s, char c) {
